d36656cd77d842b701fffa52e3219eb1c265f1e9
Damarreindra <damarihya08@gmail.com>
2024-08-10 21:12:16 +0700

    fix login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,153 +1,225 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 import {
   Box,
-  Button,
-  Container,
-  FormControl,
-  FormLabel,
-  Input,
-  Alert,
-  AlertIcon,
-  Heading,
-  Image,
-  Text,
-  VStack,
   Flex,
   Stack,
-  useToast,
-} from "@chakra-ui/react";
-import { useEffect } from "react";
-import { useAuth } from "../auth/AuthContext";
+  Heading,
+  Text,
+  Container,
+  Input,
+  Button,
+  SimpleGrid,
+  Avatar,
+  AvatarGroup,
+  useBreakpointValue,
+  IconProps,
+  Icon,
+} from '@chakra-ui/react'
+import { useAuth } from '../auth/AuthContext'
+import { useState } from 'react'
+
+const avatars = [
+  {
+    name: 'Ryan Florence',
+    url: 'https://bit.ly/ryan-florence',
+  },
+  {
+    name: 'Segun Adebayo',
+    url: 'https://bit.ly/sage-adebayo',
+  },
+  {
+    name: 'Kent Dodds',
+    url: 'https://bit.ly/kent-c-dodds',
+  },
+  {
+    name: 'Prosper Otemuyiwa',
+    url: 'https://bit.ly/prosper-baba',
+  },
+  {
+    name: 'Christian Nwamba',
+    url: 'https://bit.ly/code-beast',
+  },
+]
 
 
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Blur = (props) => {
+  return (
+    <Icon
+      width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
+      zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
+      height="560px"
+      viewBox="0 0 528 560"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <circle cx="71" cy="61" r="111" fill="#FFFF00" />
+      <circle cx="244" cy="106" r="139" fill="#48BB78" />
+      <circle cy="291" r="139" fill="#FFFF00" />
+      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED64A6" />
+      <circle cx="196.5" cy="317.5" r="101.5" fill="#48BB78" />
+      <circle cx="70.5" cy="458.5" r="101.5" fill="#ED64A6" />
+      <circle cx="426.5" cy="-0.5" r="101.5" fill="#48BB78" />
+    </Icon>
+  )
+}
+
+
+export default function Login() {
   const {login} = useAuth()
-  const toast = useToast()
-  // const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  // const { loginResult, loginError, loginSuccess } = useSelector((state) => state.UserReducer);
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   dispatch(
-  //     login({
-  //       email: email,
-  //       password: password,
-  //     })
-  //   );
-  // };
-
-  // useEffect(() => {
-  //   if (loginResult) {
-  //     toast({
-  //       position: 'bottom-left',
-  //       render: () => (
-  //         <Box color='white' p={3} bg='green.500'>
-  //          {loginSuccess}
-  //         </Box>
-  //       ),
-  //     })
-  //     navigate('/home')
-  //     setPassword("");
- 
-  //     setEmail("");
-  
-  //   } else if (loginError) {
-      
-  //     setEmail("");
-  //     setPassword("");
-  //   }
-  // }, [loginResult, loginError, navigate]);
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
 
   const handleSubmit = (e) =>{
     e.preventDefault()
     login({
       email: email,
-      password: password,
+      password: password
     })
   }
 
+
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <Container mt={5} maxW="md">
-        <Flex flex="1" justifyContent="center" alignItems="center" bg="white">
-          <Container textAlign="center">
-            <Stack spacing={6}>
-              <Box>
-                <Image
-                  src="https://user-images.githubusercontent.com/80618060/209421785-aa078881-83eb-41e3-ae28-9b05ee0d5dc0.png"
-                  style={{ width: "40px" }}
-                  mx={"auto"}
-                  srcset=""
+    <Box position={'relative'}>
+      <Container
+        as={SimpleGrid}
+        maxW={'7xl'}
+        columns={{ base: 1, md: 2 }}
+        spacing={{ base: 10, lg: 32 }}
+        py={{ base: 10, sm: 20, lg: 32 }}>
+        <Stack spacing={{ base: 10, md: 20 }}>
+          <Heading
+            lineHeight={1.1}
+            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}>
+            Tweeder Connect People{' '}
+            <Text as={'span'} bgGradient="linear(to-r, yellow.400,green.400)" bgClip="text">
+              +
+            </Text>{' '}
+            You
+          </Heading>
+          <Stack direction={'row'} spacing={4} align={'center'}>
+            <AvatarGroup>
+              {avatars.map((avatar) => (
+                <Avatar
+                  key={avatar.name}
+                  name={avatar.name}
+                  src={avatar.url}
+                  // eslint-disable-next-line react-hooks/rules-of-hooks
+                  size={useBreakpointValue({ base: 'md', md: 'lg' })}
+                  position={'relative'}
+                  zIndex={2}
+                  _before={{
+                    content: '""',
+                    width: 'full',
+                    height: 'full',
+                    rounded: 'full',
+                    transform: 'scale(1.125)',
+                    bgGradient: 'linear(to-bl, red.400,pink.400)',
+                    position: 'absolute',
+                    zIndex: -1,
+                    top: 0,
+                    left: 0,
+                  }}
                 />
-              </Box>
-              <Heading as="h1" size="xl" fontWeight="bold">
-                Login
-              </Heading>
-
-              <Button
-                href="#"
-                colorScheme="gray"
-                disabled
-                variant="outline"
-                leftIcon={
-                  <Image
-                    src="https://img.icons8.com/color/48/000000/google-logo.png"
-                    boxSize="20px"
-                  />
-                }
-              >
-                Sign up with Google
-              </Button>
-              <Heading as="h1" size="md" fontWeight="bold">
-                Or
-              </Heading>
-              {/* {loginError && (
-                <Alert status="error" mb={4}>
-                  <AlertIcon />
-                  {loginError}
-                </Alert>
-              )} */}
-              <form onSubmit={handleSubmit} id="form">
-                <VStack spacing={4}>
-                  <FormControl id="email" isRequired>
-                    <FormLabel>Email address</FormLabel>
-                    <Input
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="name@example.com"
-                    />
-                  </FormControl>
-                  <FormControl id="password" isRequired>
-                    <FormLabel>Password</FormLabel>
-                    <Input
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      placeholder="Password"
-                    />
-                  </FormControl>
-                  <Button type="submit" colorScheme="whatsapp" width="full">
-                    Login
-                  </Button>
-                </VStack>
-              </form>
+              ))}
+            </AvatarGroup>
+            <Text fontFamily={'heading'} fontSize={{ base: '4xl', md: '6xl' }}>
+              +
+            </Text>
+            <Flex
+              align={'center'}
+              justify={'center'}
+              fontFamily={'heading'}
+              fontSize={{ base: 'sm', md: 'lg' }}
+              bg={'gray.800'}
+              color={'white'}
+              rounded={'full'}
+              minWidth={useBreakpointValue({ base: '44px', md: '60px' })}
+              minHeight={useBreakpointValue({ base: '44px', md: '60px' })}
+              position={'relative'}
+              _before={{
+                content: '""',
+                width: 'full',
+                height: 'full',
+                rounded: 'full',
+                transform: 'scale(1.125)',
+                bgGradient: 'linear(to-bl, orange.400,yellow.400)',
+                position: 'absolute',
+                zIndex: -1,
+                top: 0,
+                left: 0,
+              }}>
+              YOU
+            </Flex>
+          </Stack>
+        </Stack>
+        <Stack
+          bg={'gray.50'}
+          rounded={'xl'}
+          p={{ base: 4, sm: 6, md: 8 }}
+          spacing={{ base: 8 }}
+          maxW={{ lg: 'lg' }}>
+          <Stack spacing={4}>
+            <Heading
+              color={'gray.800'}
+              lineHeight={1.1}
+              fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
+              Login
+              <Text as={'span'} bgGradient="linear(to-r, red.400,pink.400)" bgClip="text">
+                !
+              </Text>
+            </Heading>
+            <Text color={'gray.500'} fontSize={{ base: 'sm', sm: 'md' }}>
+              Get ready to share your story on our platform!
+            </Text>
+          </Stack>
+          <Box as={'form'} mt={10}>
+            <Stack spacing={4}>
+             
+              <Input
+                placeholder="firstname@lastname.io"
+                bg={'gray.100'}
+                type='email'
+                border={0}
+                color={'gray.500'}
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                _placeholder={{
+                  color: 'gray.500',
+                }}
+              />
+               <Input
+                placeholder="Password"
+                type='password'
+                bg={'gray.100'}
+                border={0}
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                color={'gray.500'}
+                _placeholder={{
+                  color: 'gray.500',
+                }}
+              />
+           
             </Stack>
-          </Container>
-        </Flex>
+            <Button
+              fontFamily={'heading'}
+              mt={8}
+              w={'full'}
+              bgGradient="linear(to-r, yellow.400,green.400)"
+              color={'white'}
+              _hover={{
+                bgGradient: 'linear(to-r, red.400,pink.400)',
+                boxShadow: 'xl',
+              }}
+              onClick={(e)=>handleSubmit(e)}
+              >
+              
+              Login
+            </Button>
+          </Box>
+          form
+        </Stack>
       </Container>
-    </motion.div>
-  );
+      <Blur position={'absolute'} top={-10} left={-10} style={{ filter: 'blur(70px)' }} />
+    </Box>
+  )
 }
-
-export default Login;
